b336bfdf4cc5091f300dce51d6b2d32f22e88356
LauBMo <laurea987@gmail.com>
2025-10-03 18:21:21 +0200

    more verbose and small fix

--- src/MobiusSphere.jl
+++ src/MobiusSphere.jl
@@ -5,11 +5,11 @@ export Mobius_to_rigid!
 
 # using UnPack: @unpack
 
-using  Nemo, NemoUtils
+using Nemo, NemoUtils
 
-@inline __normalize(z::Complex) = z
-@inline __normalize(x::Real) = x
+@inline __normalize(z::Number) = z
 @inline __normalize(z) = Nemo.complex_normal_form(z)
+
 import MobiusTransformations as MT
 
 # For rigid transformations in 3D
@@ -18,35 +18,38 @@ import MobiusTransformations as MT
 
 include("BaseMotions.jl")
 
-# Decompose Möbius to rigid transformation
+# Decompose Möbius transformation 'm' to rigid transformation 'R, T'.
 function Mobius_to_rigid!(R, G, B, proj)
     # # Map to origin sphere
-    # R = proj(z0)
-    # G = proj(z1)
-    # B = proj(z∞)
+    # R = proj(z0) # z0 = inv(m)(0)
+    # G = proj(z1) # z1 = inv(m)(1)
+    # B = proj(z∞) # z∞ = inv(m)(∞)
 
-    # Find rigid motion that moves:
+    # Find rigid motion 'R, T' that moves:
     #   B → north pole of target sphere
     #   R → projected to zero
     #   G → projected to one
+
+    # # Then 'R, T' correspoinds to Mobius such that
+    # sends z0, z1, z∞ to 0, 1, ∞; which is 'm'.
 
     # Step 1: Rotate B to north pole.
     # axis_rot1 = [-B[2],B[1],0] # = cross(B_rot, SVector(0, 0, 1))
     # angle_rot1 = acos(B[3]) # = angle(B_rot, SVector(0, 0, 1))
     points = [R, G, B]
-    print("B to north\n")
+    # print("B to north\n")
     rot1 = Btonorth(points[3])
     points = [rot1*p for p in points]
     # R, G, B = [rot1].*[R, G, B]
     # all(points[3] .== [0, 0, 1]) |> println
 
-    print("R to zero\n")
+    # print("R to zero\n")
     zr = proj(points[1])
     tr1 = Rtozero(zr)
     points = [p+tr1 for p in points]
     # R, G, B = [tr1].+[R, G, B]
 
-    print("G to one (step 1)\n")
+    # print("G to one (step 1)\n")
     tr2 = Gtoone_step1(points[3], points[2])
     points = [__normalize.(p+tr2) for p in points]
     # R, G, B = [tr2].+[R, G, B]
@@ -54,16 +57,16 @@ function Mobius_to_rigid!(R, G, B, proj)
     zg = temp_proj(points[2])
     # println("Norm of zg: ", _norm(reim(zg)...) == 1)
 
-    print("G to one (step 2)\n")
+    # print("G to one (step 2)\n")
     rot2 = Gtoone_step2(zg)
     points = [rot2*p for p in points]
 
     # map = rot2 ∘ tr2 ∘ tr1 ∘ rot1
-    print("Final Translation\n")
+    # print("Final Translation\n")
     tr = rot2*(tr1 + tr2)
-    print("Final Rotation")
+    # print("Final Rotation")
     map = rot2 * rot1
-    return map, tr, points
+    return map, tr
 end
 
 function Mobius_to_rigid(m::MT.MobiusTransformation{T}) where T
